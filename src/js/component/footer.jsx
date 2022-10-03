a0f9b812a5f25dfebcd7e4da339bf5bfafce3f59
Enmanuelph98 <enmanuelph98@gmail.com>
2022-10-03 19:18:49 +0000

    landing page with react

---
+++ src/js/component/footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className=" bg-dark text-white text-center page-footer">
+     
+        Copyright__ Your Website 2022
+    </div>
+  );
+};
+
+export default Footer;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,4 +1,10 @@
 import React from "react";
+import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
+import Footer from "./footer.jsx";
+import Navbar from "./navbar.jsx"
+import Jumbotron from "./jumbotron.jsx";
+import Card from "./card.jsx";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
@@ -6,21 +12,22 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div>
+			<Navbar />
+			<div className="container-fluid" >
+			<Jumbotron />
+
+			<div className="row">
+				<div className="col"> <Card url ="https://picsum.photos/id/3/200/300"/> </div>
+				<div className="col"> <Card url ="https://picsum.photos/id/2/200/300"/> </div>
+				<div className="col"> <Card url ="https://picsum.photos/id/1/200/300"/> </div>
+				<div className="col"> <Card url ="https://picsum.photos/id/4/200/300"/> </div>
+			</div>
+			</div>
+			<Footer/>
+		
 		</div>
-	);
+	)
 };
 
 export default Home;
